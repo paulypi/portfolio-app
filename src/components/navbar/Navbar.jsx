4e45dd6bc4300bd7e0ba9f49145a6ef3e1ddd9ae
paulypi <paolo.pierini.p@gmail.com>
2022-05-08 23:58:59 +0200

    Setup some base CSS, add assets folder and start creation of the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/Logo.png';
 import './navbar.css';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='portfolio__navbar'>
+      <div className='portfolio__navbar-links'>
+        <div className='portfolio__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='portfolio__navbar-links_container'>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#skillset">Skillset</a></p>
+          <p><a href="#whoami">Who am I</a></p>
+          <p><a href="#projects">Projects</a></p>
+          <p><a href="#Footer">Contacts</a></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
