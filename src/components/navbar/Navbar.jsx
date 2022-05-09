010c52e795d12f488e0260411c64491afe51b78e
paulypi <paolo.pierini.p@gmail.com>
2022-05-09 18:59:06 +0200

    Implement navbar component and dynamic CSS, fix index.js with createRoot (React 18).

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/Logo.png';
 import './navbar.css';
 
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#skillset">Skillset</a></p>
+  <p><a href="#whoami">Who am I</a></p>
+  <p><a href="#projects">Projects</a></p>
+  <p><a href="#Footer">Contacts</a></p>
+  </>
+)
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='portfolio__navbar'>
       <div className='portfolio__navbar-links'>
@@ -12,12 +24,29 @@ const Navbar = () => {
           <img src={logo} alt='logo' />
         </div>
         <div className='portfolio__navbar-links_container'>
-          <p><a href="#home">Home</a></p>
-          <p><a href="#skillset">Skillset</a></p>
-          <p><a href="#whoami">Who am I</a></p>
-          <p><a href="#projects">Projects</a></p>
-          <p><a href="#Footer">Contacts</a></p>
+          <Menu />
         </div>
+      </div>
+      <div className='portfolio__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+      <div className='portfolio__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color="#FFF" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#FFF" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='portfolio__navbar-menu_container scale-up-right'>
+            <div className='portfolio__navbar-menu_container-links'>
+              <Menu />
+              <div className='portfolio__navbar-menu_container-links-sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
