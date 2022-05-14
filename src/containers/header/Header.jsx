61fffa087cac407321ba5069d597b758f1dfd74f
paulypi <paolo.pierini.p@gmail.com>
2022-05-14 22:28:58 +0200

    Add homepage

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,22 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='portfolio__header' id='#home'>
+      <div className='portfolio__header-content'>
+        <h1 className='gradient__text'>Hi, i'm Paolo</h1>
+        <p> Back-end Developer | Studing as Data engineer </p>
+
+        <div className='portfolio__header-content_github'>
+            <h2 className='gradient__text'>GitHub account:</h2>
+            <button>Check my GitHub</button>
+        </div>
+        
+        <div className='portfolio__header-content_contact'>
+          <p>To contact me, take a look to <a href="#footer" className='gradient__text'>this section</a></p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
