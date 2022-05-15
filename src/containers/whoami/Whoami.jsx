9109a08477d6d186aa4eb2de3623d4122c0cad6a
paulypi <paolo.pierini.p@gmail.com>
2022-05-15 10:44:54 +0200

    Add whoami section

--- src/containers/whoami/Whoami.jsx
+++ src/containers/whoami/Whoami.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Article } from '../../components';
 import './whoami.css';
 
 const Whoami = () => {
   return (
-    <div>
-      Whoami
+    <div className='portfolio__whoami section__padding' id='whoami'>
+      <div className='portfolio__whoami-container'>
+        <div className='portfolio__whoami-container_title gradient__text'>
+          <h1>Who am I!</h1>
+        </div>
+        <div className='portfolio__whoami-container_text'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit provident delectus facilis asperiores, possimus nostrum unde minus ut veniam voluptatibus, sequi non quas porro natus reprehenderit? Dignissimos voluptate harum itaque.</p>
+        </div>
+      </div>
     </div>
   )
 }
