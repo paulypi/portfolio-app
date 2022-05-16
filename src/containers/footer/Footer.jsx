0c6a26f65ce9717a0f1f5b11d45262bdcfa2ced6
paulypi <paolo.pierini.p@gmail.com>
2022-05-16 02:22:58 +0200

    Change and fix colors

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -5,25 +5,23 @@ import './footer.css';
 const Footer = () => {
   return (
     <div className='portfolio__footer section__padding' id='footer'>
-      <div className='portfolio__footer-heading'>
-        <h1 className='gradient__text'>Contacts</h1>
-        <p>Feel free to use the form below or check my connections</p>
-      </div>
-
       <div className='portfolio__footer-links'>
+        <div className='portfolio__footer-links_heading'>
+          <h1 className='gradient__text'>Contacts</h1>
+          <p>Feel free to use the form below or check my connections</p>
+        </div>
         <div className='portfolio__footer-links_form'>
           <input type="email" placeholder='Emamil address' />
           <textarea name="text" cols="40" rows="5" placeholder='Insert your text here'></textarea>
           <button type='button'>Send</button>
         </div>
-
-        <div className='portfolio__footer-links_connections'>
+      </div>
+      <div className='portfolio__footer-connections'>
           <h1 className='gradient__text'>Connections</h1>
           <a href="#footer"><img src={gmail} alt="gmail img" /></a>
           <a href="#footer"><img src={twitter} alt="twitter img" /></a>
           <a href="#footer"><img src={gitHub} alt="github img" /></a>
         </div>
-      </div>
     </div>
   )
 }
